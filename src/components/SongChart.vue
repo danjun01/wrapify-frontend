3eb69c760d7edfb2d5c9915353415738f558a418
danjun01 <danjun01@users.noreply.github.com>
2025-02-17 22:13:43 -0500

    Created SongChart component and added to HomeView

---
+++ src/components/SongChart.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+    Table,
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell
+} from "@/components/ui/table"
+</script>
+
+<template>
+    <Table class="text-xl">
+        <TableHeader>
+            <TableRow>
+                <TableHead class="text-center">Rank</TableHead>
+                <TableHead>Song</TableHead>
+                <TableHead>Artist</TableHead>
+                <TableHead>Plays</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow>
+                <TableCell class="text-center">
+                    1
+                </TableCell>
+                <TableCell class="flex items-center">
+                    <img class="aspect-square w-20" src="../assets/imgs/I-don't-think-that-I-like-her.jpg" />
+                    <h2 class="pl-10 text-align-center">
+                        I Don't Think I Like Her
+                    </h2>
+                </TableCell>
+                <TableCell>
+                    Charlie Puth
+                </TableCell>
+                <TableCell>
+                    6
+                </TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+</template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-
+import SongChart from '@/components/SongChart.vue';
 </script>
 
 <template>
 
-    <div>
-        test
+    <div class="flex flex-col items-center pt-20 px-[20%]">
+        <h1 class="text-6xl font-extrabold">My Top Songs</h1>
+        <SongChart class="mt-20" filter="" />
+
     </div>
 </template>
