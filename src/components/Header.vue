f9f86477ccede64470b972503680f4fd2940b918
danjun01 <danjun01@users.noreply.github.com>
2025-02-19 03:18:49 -0500

    styling changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,7 +18,7 @@ import { onBeforeMount, ref, watch } from 'vue';
 <template>
   <div class="flex items-center justify-between w-full bg-zinc-900 text-white py-5 pl-20 pr-6">
       <RouterLink to="/" replace>
-        <h1 class="font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-violet-400">Wrapify</h1>
+        <h1 class="font-bold text-3xl">Wrapify</h1>
       </RouterLink>
 
       <OAuthButton />
