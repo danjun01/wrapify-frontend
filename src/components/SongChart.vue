87771aa7bc16e269b1dd85bcd6e5f6a201904a35
danjun01 <danjun01@users.noreply.github.com>
2025-02-19 03:18:30 -0500

    Added getTopTracks function to collect user top track data.
    Changed Table to dynamically generate rows based on returned tracks

--- src/components/SongChart.vue
+++ src/components/SongChart.vue
@@ -7,10 +7,34 @@ import {
     TableBody,
     TableCell
 } from "@/components/ui/table"
+import axios from "axios";
+import { onMounted, ref } from 'vue';
+import Button from "./ui/button/Button.vue";
+
+const access_token = ref(localStorage.getItem('access_token') || '');
+const topTracks = ref([]);
+const getTopTracks = async () => {
+    const res = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+        params: {
+            'time_range': 'short_term',
+            'limit': '15'
+        },
+        headers: {
+            "Authorization": `Bearer ${access_token.value}`
+        }
+    });
+    console.log('RESPONSE \n-------------------------------------------\n\n',res.data);
+    
+    topTracks.value = res.data['items']
+    console.log('TOP Tracks \n-------------------------------------------\n\n',topTracks.value);
+}
+
+onMounted(getTopTracks);
+
 </script>
 
 <template>
-    <Table class="text-xl">
+    <Table class="text-2xl font-semibold">
         <TableHeader>
             <TableRow>
                 <TableHead class="text-center">Rank</TableHead>
@@ -20,18 +44,18 @@ import {
             </TableRow>
         </TableHeader>
         <TableBody>
-            <TableRow>
+            <TableRow v-for="(track, index) in topTracks" :key="index">
                 <TableCell class="text-center">
-                    1
+                    {{index + 1}}
                 </TableCell>
                 <TableCell class="flex items-center">
-                    <img class="aspect-square w-20" src="../assets/imgs/I-don't-think-that-I-like-her.jpg" />
+                    <img class="aspect-square w-40" :src="track['album']['images'][0]['url']" />
                     <h2 class="pl-10 text-align-center">
-                        I Don't Think I Like Her
+                        {{ track['name'] }}
                     </h2>
                 </TableCell>
                 <TableCell>
-                    Charlie Puth
+                    {{ track['artists'][0]['name'] }}
                 </TableCell>
                 <TableCell>
                     6
