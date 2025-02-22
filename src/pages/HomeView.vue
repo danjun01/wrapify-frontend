d2957e1dab992b6231ea1923c3e6ccfa1dd271b4
danjun01 <danjun01@users.noreply.github.com>
2025-02-22 01:20:11 -0500

    Refactored to use authStore

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import SongChart from '@/components/SongChart.vue';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
 </script>
 
 <template>
 
-    <div class="flex flex-col items-center pt-20 px-[20%]">
+    <div v-if="authStore.isAuthenticated" class="flex flex-col items-center pt-20 px-[20%]">
         <h1 class="text-6xl font-extrabold">My Top Songs</h1>
         <SongChart class="mt-20" filter="" />
 
+    </div>
+    <div v-else class="flex flex-col items-center pt-20 px-[20%]">
+        Else
     </div>
 </template>
