d7d1c3a4880467ec89c957fd1f88f66d5e02db7c
danjun01 <danjun01@users.noreply.github.com>
2025-02-22 01:19:31 -0500

    Refactored to use authStore + store user data after pulling from User's Spotify profile using Spotify API

--- src/pages/SpotifyAuthRedirect.vue
+++ src/pages/SpotifyAuthRedirect.vue
@@ -2,7 +2,9 @@
 import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
+import { useAuthStore } from '@/stores/auth';
 
+const authStore = useAuthStore();
 
 const route = useRoute();
 const router = useRouter();
@@ -11,15 +13,20 @@ const authCode = ref('')
 const error = ref('')
 
 // Access the code or error immediately when the route changes
+
 if (route.query.code) {
   authCode.value = route.query.code.toString();
   console.log('auth code: ', authCode.value);
-  exchangeAuthCodeForToken(authCode.value); // Exchange code for token
 
+  exchangeAuthCodeForToken(authCode.value) // Exchange code for token
+    .then( () => getUserProfile())
+    .catch( error => console.log(error));
+
+  router.push('/');
 } else if (route.query.error) {
   error.value = route.query.error.toString();
+  console.log(error.value);
 }
-
 
 
 // Function to exchange the authorization code for an access token
@@ -28,15 +35,27 @@ async function exchangeAuthCodeForToken(code: string) {
     console.log('requesting token');
     const response = await axios.post('http://localhost:8000/api/spotify/token/', { code: code });
     console.log('Token response:', response.data);
-    localStorage.setItem('access_token', response.data.access_token);
-    localStorage.setItem('refresh_token', response.data.refresh_token);
-    router.push('/')
+    authStore.setTokens(response.data.access_token, response.data.refresh_token);
 
     // Do something with the token response
   } catch (err) {
-    console.error('Error exchanging auth code for token:', err)
+    console.error('Error exchanging auth code for token:', err);
   }
 }
+
+async function getUserProfile() {
+  try {
+    console.log('Getting user profile');
+    const response = await axios.get('http://localhost:8000/api/user/', {
+      params: { access_token: authStore.access_token }
+    });
+    console.log(response);
+    authStore.login(response.data.spotify_display_name, response.data.spotify_id, response.data.profile_image_url);
+  }
+  catch (err) {
+    console.log('Error getting user profile:', err);
+  }
+};
 </script>
 
 <template>
