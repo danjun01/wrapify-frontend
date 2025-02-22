99e8c655918e26659af3da944f7e9b895f2b5468
danjun01 <danjun01@users.noreply.github.com>
2025-02-22 01:16:29 -0500

    Created a component for a dropdown account menu

---
+++ src/components/AccountMenu.vue
@@ -0,0 +1,54 @@
+<script setup lang='ts'>
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const handleLogout = () => {
+  authStore.logout();
+}
+
+const pfp = authStore.profile_image_url as string;
+console.log("pfp url:", pfp)
+</script>
+
+<template>
+  <NavigationMenu>
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger class="flex gap-3">
+          <Avatar>
+            <AvatarImage :src="pfp" alt="@pfp" />
+            <AvatarFallback>Account</AvatarFallback>
+          </Avatar>
+        </NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul>
+            <li>
+              <NavigationMenuLink >
+                <a>
+                  <div>Account</div>
+                </a>
+              </NavigationMenuLink>
+            </li>
+
+            <li>
+              <NavigationMenuLink>
+                <div @click="handleLogout">Logout</div>
+              </NavigationMenuLink>
+            </li>
+
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  </NavigationMenu>
+</template>
