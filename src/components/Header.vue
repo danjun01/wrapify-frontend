8ed95067f2f348613624489dbf67cd6863a78eab
Daniel Jun <dhj190001@utdallas.edu>
2025-02-26 02:14:39 -0500

    Changes to color theme

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,9 +9,9 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <div class="flex items-center justify-between w-full bg-zinc-900 text-white py-5 pl-20 pr-6">
+  <div class="flex items-center justify-between w-full text-platinum py-5 pl-20 pr-6">
       <RouterLink to="/" replace>
-        <h1 class="font-bold text-3xl">Wrapify</h1>
+        <h1 class="font-bold text-3xl text-orange-web">Wrapify</h1>
       </RouterLink>
 
       <OAuthButton v-if="!authStore.isAuthenticated" />
